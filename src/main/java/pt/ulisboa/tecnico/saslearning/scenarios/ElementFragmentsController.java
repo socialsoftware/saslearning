69cbbbdaa3a95fc2a9d1dfe8cc4ba4c703e72abf
Catarina Isabel Carvalho Santana <catarina.santana@gmail.com>
2015-07-08 19:09:25 +0100

    expanding the fragmentManager view

--- src/main/java/pt/ulisboa/tecnico/saslearning/scenarios/ElementFragmentsController.java
+++ src/main/java/pt/ulisboa/tecnico/saslearning/scenarios/ElementFragmentsController.java
@@ -1,16 +1,9 @@
 package pt.ulisboa.tecnico.saslearning.scenarios;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.view.RedirectView;
 
 import pt.ist.fenixframework.Atomic;
@@ -24,51 +17,6 @@ import pt.ulisboa.tecnico.saslearning.utils.Utils;
 @Controller
 public class ElementFragmentsController {
 	Utils utils = new Utils();
-	
-	@RequestMapping(value="/removeSyntax/{docId}/{frag}/{toUnlink}")
-	public RedirectView removeFragmentSyntax(@PathVariable String docId, @PathVariable String frag, @PathVariable String toUnlink) {
-		ElementFragment src = FenixFramework.getDomainObject(frag);
-		ElementFragment unlink = FenixFramework.getDomainObject(toUnlink);
-		unlinkDifferentFragments(src, unlink);
-		RedirectView rv = new RedirectView("/linkManager/" + docId + "/" + frag);
-		return rv;
-	}
-	
-	@RequestMapping(value="/addFragmentSyntax/{docId}/{toLink}/{linked}")
-	public RedirectView addFragmentSyntax(@PathVariable String docId, @PathVariable String toLink, @PathVariable String linked) {
-		ElementFragment fragToLink = FenixFramework.getDomainObject(toLink);
-		ElementFragment destFrag = FenixFramework.getDomainObject(linked);
-		linkDifferentFragments(fragToLink, destFrag);
-		RedirectView rv = new RedirectView("/linkManager/" + docId + "/" + toLink);
-		return rv;
-	}
-	
-	@RequestMapping(value="/linkManager/{docId}/{fragId}", method=RequestMethod.GET)
-	public String linkManager(@PathVariable String docId, @PathVariable String fragId, Model m) {
-		ElementFragment e = FenixFramework.getDomainObject(fragId);
-		Document d = FenixFramework.getDomainObject(docId);
-		Set<ElementFragment> heads = d.getFragmentSet();
-		Map<String, List<ElementFragment>> possibleLinks = new HashMap<String, List<ElementFragment>>();
-		List<String> possibilities = e.possibleConnections();
-		m.addAttribute("chainToLink", e);
-		for(String tag : possibilities) { //preencher o mapa com as possiveis ligaçoes
-			for(ElementFragment elem : heads) {
-				if(elem.getName().equals(tag) && !elem.hasConnections()) {
-					if(possibleLinks.get(tag) != null) {
-						possibleLinks.get(tag).add(elem);
-					}else {
-						List<ElementFragment> list = new ArrayList<ElementFragment>();
-						list.add(elem);
-						possibleLinks.put(tag, list);
-					}
-				}
-			}
-		}
-		m.addAttribute("possibleLinks", possibleLinks);
-		m.addAttribute("docId", docId);
-		m.addAttribute("annId", e.getAnnotation().getExternalId());
-		return "linksManager";
-	}
 	
 	@RequestMapping(value = "/fragmentManager/{docId}/{annotationId}")
 	public String fragmentManager(Model m,
@@ -101,25 +49,17 @@ public class ElementFragmentsController {
 		RedirectView rv = new RedirectView("/fragmentManager/" + docId+"/" + toUnlink.getAnnotation().getExternalId());
 		return rv;
 	}
-	
-	@Atomic(mode=TxMode.WRITE)
-	private void unlinkDifferentFragments(ElementFragment src,
-			ElementFragment unlink) {
-		src.unlink(unlink);
-	}
-
-	@Atomic(mode=TxMode.WRITE)
-	private void linkDifferentFragments(ElementFragment fragToLink, ElementFragment destFrag) {
-		fragToLink.connect(destFrag);
-	}
 
 	@Atomic(mode=TxMode.WRITE)
 	private void unlinkFragments(ElementFragment toUnlink, ElementFragment head) {
 		if(toUnlink.getExternalId().equals(head.getExternalId())) { // remover a cabeça da lista
 			ElementFragment child = toUnlink.getChild();
+			if(toUnlink.hasConnections()) {
+				toUnlink.passConnectionsToChild();
+			}
 			child.setParent(null);
 			child.setDocument(toUnlink.getDocument());
-			if(child.getChild() == null) {
+			if(child.getChild() == null && !child.hasConnections()) {
 				child.setLinked(false);
 			}
 			toUnlink.setChild(null);
@@ -137,7 +77,7 @@ public class ElementFragmentsController {
 			toUnlink.setParent(null);
 			toUnlink.setChild(null);
 			toUnlink.setLinked(false);
-			if(head.getChild() == null) { //se por acaso só estavam 2 fragmentos ligados e removemos 1
+			if(head.getChild() == null && !head.hasConnections()) { //se por acaso só estavam 2 fragmentos ligados e removemos 1
 				head.setLinked(false);
 			}
 			
