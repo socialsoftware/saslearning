db3aa0fca1c3cc7c3be06fba0aef8dae57bb97fa
David Duarte <david.duarte@tecnico.ulisboa.pt>
2017-10-26 23:52:53 +0100

    Setup auth services
    Close #2

---
+++ src/main/scala/pt/ulisboa/tecnico/socialsoftware/services/OAuthService.scala
@@ -0,0 +1,62 @@
+package pt.ulisboa.tecnico.socialsoftware.services
+
+import java.net.URI
+
+import io.circe.{Decoder, HCursor, Json}
+import pt.ulisboa.tecnico.socialsoftware.domain.oauth.{Token, Provider}
+import pt.ulisboa.tecnico.socialsoftware.domain.User
+
+case class OAuthService(client: Provider) {
+
+  val redirectUri: URI = URI.create(
+    s"http://localhost:8081/${client.name.toLowerCase.trim}/callback"
+  )
+
+  def userFromOAuth(authInfo: Token): Option[User] = {
+    import scalaj.http.Http
+    import io.circe.parser.parse
+
+    val response = Http(client.userProfileEndpoint.toString)
+      .header("Authorization", s"Bearer ${authInfo.accessToken}")
+      .asString
+      .body
+
+    val doc = parse(response).getOrElse(Json.Null)
+
+    client.getUser(doc)
+  }
+
+  def requestAccessToken(code: String): Option[Token] = {
+    import scalaj.http.Http
+    import io.circe.parser._
+
+    val response = Http(client.accessTokenUrl.toString)
+      .postForm
+      .param("client_id", client.id)
+      .param("client_secret", client.secret)
+      .param("redirect_uri", redirectUri.toString)
+      .param("code", code)
+      //      .param("state", "TODO")
+      .param("grant_type", "authorization_code")
+      .asString
+      .body
+
+    // FIXME authorization_code and scope
+    // FIXME handle unauthorization
+
+    parse(response)
+      .toOption
+      .flatMap(_.as[Token].toOption)
+  }
+
+  // TODO: private def refreshAccessToken(): Unit = ???
+
+  private implicit val decodeOAuthInfo: Decoder[Token] = (c: HCursor) => for {
+    accessToken <- c.downField("access_token").as[String]
+    refreshToken <- c.downField("refresh_token").as[Option[String]]
+    expiresIn <- c.downField("expires_in").as[Int]
+  } yield {
+    Token(accessToken, refreshToken, expiresIn, client) //FIXME now + expiresIn
+  }
+
+}
