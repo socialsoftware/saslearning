7b07024c2c1222c36240912d27d4567dd789bb6f
David Duarte <david.duarte@tecnico.ulisboa.pt>
2017-12-18 01:38:45 +0000

    WIP add tests for threads in documents

---
+++ src/test/scala/pt/ulisboa/tecnico/socialsoftware/saslearning/domain/DocumentSpec.scala
@@ -0,0 +1,27 @@
+package pt.ulisboa.tecnico.socialsoftware.saslearning.domain
+
+import org.scalatest.{EitherValues, Matchers, WordSpec}
+
+class DocumentSpec extends WordSpec
+  with Matchers
+  with EitherValues {
+
+  "Posting a comment" should {
+    "create a thread when it doesn't exist" in {
+
+    }
+    "add it to the thread" in {
+
+    }
+  }
+
+  "Deleting a comment" should {
+    "remove it from the thread" in {
+
+    }
+    "delete the thread" in {
+
+    }
+  }
+
+}
