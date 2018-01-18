4e402385bd0d2e014843eb74fe330c7e5e1d5c50
David Duarte <david.duarte@tecnico.ulisboa.pt>
2018-01-18 23:58:46 +0000

    Add more tests for Documents

--- src/test/scala/pt/ulisboa/tecnico/socialsoftware/saslearning/domain/collaboration/DocumentSpec.scala
+++ src/test/scala/pt/ulisboa/tecnico/socialsoftware/saslearning/domain/collaboration/DocumentSpec.scala
@@ -5,6 +5,7 @@ import javax.mail.internet.InternetAddress
 
 import eu.timepit.refined.api.Refined
 import eu.timepit.refined.auto._
+import eu.timepit.refined.types.string.NonEmptyString
 import org.scalatest.{EitherValues, Matchers, WordSpec}
 import pt.ulisboa.tecnico.socialsoftware.saslearning.domain.User
 
@@ -15,13 +16,42 @@ class DocumentSpec extends WordSpec
   with EitherValues {
 
   private val user = User(None, username = "jdoe", email = new InternetAddress("john.doe@example.org"), displayName = "John Doe")
+
+  private val defaultUri = URI.create("http://example.org")
+  private val defaultTitle: NonEmptyString = Refined.unsafeApply("The history of Art")
+  private val defaultContent: NonEmptyString = Refined.unsafeApply("Content goes here")
+
+  private val defaultDocument = Document(None, defaultTitle, defaultUri, defaultContent, user, None)
+
   private val question = Question("What is art?", user)
-  private val defaultDocument = Document(None, "The history of Art", URI.create("http://example.org"), "Content goes here", user, None)
   private val documentWithQuestion = defaultDocument.copy(thread = Some(Thread(Refined.unsafeApply(Seq(question)))))
 
   private def assertRight(expected: Document, actual: Either[String, Document]) = {
     actual should be('right)
     actual.right.value should be(expected)
+  }
+
+  "A document" should {
+    "have a title" when {
+      "is empty" in {
+        val document = Document.fromUnsafe(None, "", defaultUri, defaultContent, user, None)
+        document should be ('left)
+      }
+      "is not empty" in {
+        val document = Document.fromUnsafe(None, "The history of Art", defaultUri, defaultContent, user, None)
+        assertRight(expected = defaultDocument, actual = document)
+      }
+    }
+    "have content" when {
+      "is empty" in {
+        val document = Document.fromUnsafe(None, defaultTitle, defaultUri, "", user, None)
+        document should be ('left)
+      }
+      "is not empty" in {
+        val document = Document.fromUnsafe(None, defaultTitle, defaultUri, "Content goes here", user, None)
+        assertRight(expected = defaultDocument, actual = document)
+      }
+    }
   }
 
   "Posting a comment" should {
