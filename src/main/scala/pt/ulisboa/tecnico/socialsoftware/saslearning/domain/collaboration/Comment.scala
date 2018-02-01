cd222ba6d65b1c873854b6b00e571f0f567aa4b3
David Duarte <david.duarte@tecnico.ulisboa.pt>
2018-02-01 16:34:32 +0000

    Test decoder for Comment (#35)

--- src/main/scala/pt/ulisboa/tecnico/socialsoftware/saslearning/domain/collaboration/Comment.scala
+++ src/main/scala/pt/ulisboa/tecnico/socialsoftware/saslearning/domain/collaboration/Comment.scala
@@ -11,11 +11,19 @@ sealed trait Comment {
 object Comment {
 
   import io.circe.{Decoder, Encoder}
-  import io.circe.generic.semiauto.{deriveDecoder, deriveEncoder}
+  import io.circe.generic.extras._
+  import io.circe.generic.extras.semiauto.{deriveDecoder, deriveEncoder}
   import io.circe.refined._
 
-  implicit val decodeJson: Decoder[Comment] = deriveDecoder
-  implicit val encodeJson: Encoder[Comment] = deriveEncoder
+  implicit val config: Configuration = Configuration
+    .default
+    .withSnakeCaseConstructorNames
+    .withSnakeCaseMemberNames
+    .withDiscriminator("type")
+
+  implicit val decodeJson: Decoder[Comment] = deriveDecoder[Comment]
+  implicit val encodeJson: Encoder[Comment] = deriveEncoder[Comment]
+
 }
 
 final case class Question(content: NonEmptyString, author: User) extends Comment
@@ -24,17 +32,17 @@ final case class Definition(content: NonEmptyString, author: User) extends Comme
 final case class NeedMoreInformation(content: NonEmptyString, author: User) extends Comment
 
 object Question {
-  def fromUnsafe(content: String, author: User): Either[String, Comment] = fromString(content)(Question(_, author))
+  def fromUnsafe(content: String, author: User): Either[String, Question] = fromString(content)(Question(_, author))
 }
 
 object Answer {
-  def fromUnsafe(content: String, author: User): Either[String, Comment] = fromString(content)(Answer(_, author))
+  def fromUnsafe(content: String, author: User): Either[String, Answer] = fromString(content)(Answer(_, author))
 }
 
 object Definition {
-  def fromUnsafe(content: String, author: User): Either[String, Comment] = fromString(content)(Definition(_, author))
+  def fromUnsafe(content: String, author: User): Either[String, Definition] = fromString(content)(Definition(_, author))
 }
 
 object NeedMoreInformation {
-  def fromUnsafe(content: String, author: User): Either[String, Comment] = fromString(content)(NeedMoreInformation(_, author))
+  def fromUnsafe(content: String, author: User): Either[String, NeedMoreInformation] = fromString(content)(NeedMoreInformation(_, author))
 }
--- src/test/scala/pt/ulisboa/tecnico/socialsoftware/saslearning/domain/collaboration/CommentSpec.scala
+++ src/test/scala/pt/ulisboa/tecnico/socialsoftware/saslearning/domain/collaboration/CommentSpec.scala
@@ -2,7 +2,19 @@ package pt.ulisboa.tecnico.socialsoftware.saslearning.domain.collaboration
 
 import pt.ulisboa.tecnico.socialsoftware.saslearning.domain.UnitSpec
 
+import eu.timepit.refined.api.Refined
+import io.circe.syntax._
+
 class CommentSpec extends UnitSpec {
+
+  private val question = "Can you explain this?"
+  private val answer = "This is a pie"
+  private val definition = "The number PI is a mathematical constant"
+  private val needMoreInformation = "Clarify why 80 is the default port for HTTP"
+
+  private def commentToJsonString(comment: Comment, category: String): String =
+    s"""{"content":"${comment.content}","author":${user.asJson.noSpaces},"type":"$category"}""".stripMargin
+
 
   private def assertRight(expected: String, actual: Either[String, Comment]) = {
     actual should be('right)
@@ -16,8 +28,7 @@ class CommentSpec extends UnitSpec {
       assertLeft(comment)
     }
     "have content" in {
-      val content = "Can you explain this?"
-      assertRight(expected = content, actual = Question.fromUnsafe(content, user))
+      assertRight(expected = question, actual = Question.fromUnsafe(question, user))
     }
   }
 
@@ -27,8 +38,7 @@ class CommentSpec extends UnitSpec {
       assertLeft(comment)
     }
     "have content" in {
-      val content = "Can you explain this?"
-      assertRight(expected = content, actual = Answer.fromUnsafe(content, user))
+      assertRight(expected = answer, actual = Answer.fromUnsafe(answer, user))
     }
   }
 
@@ -38,8 +48,7 @@ class CommentSpec extends UnitSpec {
       assertLeft(comment)
     }
     "have content" in {
-      val content = "The number π is a mathematical constant"
-      assertRight(expected = content, actual = Definition.fromUnsafe(content, user))
+      assertRight(expected = definition, actual = Definition.fromUnsafe(definition, user))
     }
   }
 
@@ -49,8 +58,30 @@ class CommentSpec extends UnitSpec {
       assertLeft(comment)
     }
     "have content" in {
-      val content = "Clarify why 80 is the default port for HTTP"
-      assertRight(expected = content, actual = NeedMoreInformation.fromUnsafe(content, user))
+      assertRight(expected = needMoreInformation, actual = NeedMoreInformation.fromUnsafe(needMoreInformation, user))
+    }
+  }
+
+  "Exporting a comment to JSON" should {
+    "contain the type information" when {
+      "is a question" in {
+        val comment: Comment = Question(Refined.unsafeApply(question), user)
+        assert(comment.asJson.noSpaces == commentToJsonString(comment, "question"))
+      }
+      "is an answer" in {
+        val comment: Comment = Answer(Refined.unsafeApply(answer), user)
+        assert(comment.asJson.noSpaces == commentToJsonString(comment, "answer"))
+
+      }
+      "is a definition" in {
+        val comment: Comment = Definition(Refined.unsafeApply(definition), user)
+        assert(comment.asJson.noSpaces == commentToJsonString(comment, "definition"))
+
+      }
+      "is a request for more information" in {
+        val comment: Comment = NeedMoreInformation(Refined.unsafeApply(needMoreInformation), user)
+        assert(comment.asJson.noSpaces == commentToJsonString(comment, "need_more_information"))
+      }
     }
   }
 }
