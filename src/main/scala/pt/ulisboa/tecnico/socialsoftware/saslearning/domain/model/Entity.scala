64235b5181e4b272744e81edbdfaeaad2cde9902
David Duarte <david.duarte@tecnico.ulisboa.pt>
2018-06-27 16:10:19 +0100

    Rename connection rules to connections

---
+++ src/main/scala/pt/ulisboa/tecnico/socialsoftware/saslearning/domain/model/Entity.scala
@@ -0,0 +1,3 @@
+package pt.ulisboa.tecnico.socialsoftware.saslearning.domain.model
+
+class Entity {}
--- src/main/scala/pt/ulisboa/tecnico/socialsoftware/saslearning/domain/model/EntityType.scala
+++ src/main/scala/pt/ulisboa/tecnico/socialsoftware/saslearning/domain/model/EntityType.scala
@@ -2,13 +2,15 @@ package pt.ulisboa.tecnico.socialsoftware.saslearning.domain.model
 
 import eu.timepit.refined.types.string.NonEmptyString
 
-case class EntityType(name: NonEmptyString, rules: Map[ConnectionRule, EntityType] = Map.empty)
+case class EntityType(name: NonEmptyString,
+                      connections: Map[ConnectionRule, EntityType] = Map.empty)
 
 object EntityType {
-  def apply(name: String, rules: Map[ConnectionRule, EntityType]): Either[String, EntityType] =
+  def apply(name: String,
+            connections: Map[ConnectionRule, EntityType]): Either[String, EntityType] =
     for {
       nonEmptyName <- NonEmptyString.from(name)
-    } yield new EntityType(nonEmptyName, rules)
+    } yield new EntityType(nonEmptyName, connections)
 
   def apply(name: String): Either[String, EntityType] = EntityType(name, Map.empty)
 }
