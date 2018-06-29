70461989c31cf837dc92a3e6ede6d92ccfe462ec
David Duarte <david.duarte@tecnico.ulisboa.pt>
2018-06-29 18:34:42 +0100

    Remove multiplicity from EntityType

--- src/main/scala/pt/ulisboa/tecnico/socialsoftware/saslearning/domain/model/EntityType.scala
+++ src/main/scala/pt/ulisboa/tecnico/socialsoftware/saslearning/domain/model/EntityType.scala
@@ -2,15 +2,11 @@ package pt.ulisboa.tecnico.socialsoftware.saslearning.domain.model
 
 import eu.timepit.refined.types.string.NonEmptyString
 
-case class EntityType(name: NonEmptyString,
-                      connections: Map[ConnectionRule, EntityType] = Map.empty)
+case class EntityType(name: NonEmptyString)
 
 object EntityType {
-  def apply(name: String,
-            connections: Map[ConnectionRule, EntityType]): Either[String, EntityType] =
+  def apply(name: String): Either[String, EntityType] =
     for {
-      nonEmptyName <- NonEmptyString.from(name)
-    } yield new EntityType(nonEmptyName, connections)
-
-  def apply(name: String): Either[String, EntityType] = EntityType(name, Map.empty)
+      checkedName <- NonEmptyString.from(name)
+    } yield EntityType(checkedName)
 }
--- src/test/scala/pt/ulisboa/tecnico/socialsoftware/saslearning/domain/model/EntityTypeSpec.scala
+++ src/test/scala/pt/ulisboa/tecnico/socialsoftware/saslearning/domain/model/EntityTypeSpec.scala
@@ -1,7 +1,5 @@
 package pt.ulisboa.tecnico.socialsoftware.saslearning.domain.model
 
-import eu.timepit.refined.types.numeric.NonNegInt
-import eu.timepit.refined.types.string.NonEmptyString
 import pt.ulisboa.tecnico.socialsoftware.saslearning.domain.UnitSpec
 
 class EntityTypeSpec extends UnitSpec {
@@ -14,29 +12,12 @@ class EntityTypeSpec extends UnitSpec {
 
       entityType should be('right)
       entityType.right.value.name.value shouldEqual name
-      entityType.right.value.connections shouldBe empty
     }
   }
 
   it can {
     "not have a name" in {
       EntityType("") should be('left)
-    }
-    "have connections rules" in {
-      val connectionRule =
-        ConnectionRule(
-          NonEmptyString.unsafeFrom("Has Tactic"),
-          new Multiplicity(NonNegInt.unsafeFrom(1), Some(NonNegInt.unsafeFrom(1)))
-        )
-
-      val tactic = EntityType(NonEmptyString.unsafeFrom("Tactic"))
-
-      val entityType =
-        EntityType(name, Map(connectionRule -> tactic))
-
-      entityType should be('right)
-      entityType.right.value.name.value shouldEqual name
-      entityType.right.value.connections.get(connectionRule) shouldBe Some(tactic)
     }
   }
 
